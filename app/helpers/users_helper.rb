29a3550c19ed06cd6a95603f940e73359f2facaf
Paul Laudeman <paul.laudeman@gmail.com>
2013-07-17 19:01:17 -0400

    Updates

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,18 @@
 module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user
-  def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+#  def gravatar_for(user, options = { size: 50 })
+#    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+#    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+#    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+#  end
+  
+  def gravatar_for email, options = {}
+    options = {:alt => 'avatar', :class => 'avatar', :size => 80}.merge! options
+    id = Digest::MD5::hexdigest email.strip.downcase
+    url = 'https://secure.gravatar.com/avatar/' + id + '.jpg?s=' + options[:size].to_s
+    options.delete :size
+    image_tag url, options
   end
+
 end
